eb5f7267d20ddaaea00b56aa8291e9197e0d2001
turgaykirkil <turgaykirkil@gmail.com>
2023-09-23 14:36:47 +0300

    Initial Commit

---
+++ SumMach/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SumMach
+//
+//  Created by Turgay KIRKIL on 23.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
